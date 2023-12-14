7afb0bc3c9794c6f024ec32467b0285e5e0f9e08
jteitelb <7592143+jteitelb@users.noreply.github.com>
2023-12-14 13:41:19 -0500

    add ratelimiter

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -14,6 +14,16 @@ const filterUserForClient = (user: User) => ({
   imageUrl: user.imageUrl,
 });
 
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
+
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -67,6 +77,14 @@ export const postRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+        });
+      }
+
       const post = await ctx.db.post.create({
         data: {
           content: input.content,
